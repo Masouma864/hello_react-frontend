7d9fae9c783ffd9eb8163be8029e13c0c8a65b56
masouma <masouma2020@gmail.com>
2023-03-09 19:28:30 +0430

    Create a greeting component to display the greetings.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import { fetchRandomGreeting } from "./api/fetchGreetings";
+import Greetings from "./components/Greetings";
+
+
+const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchRandomGreeting())
+  }, [dispatch])
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <Routes>
+       <Route path="/" element={<Greetings />}/>
+    </Routes>
+  )
 }
 
 export default App;
